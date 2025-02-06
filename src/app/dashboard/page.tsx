667d62e0f6ab91106c42db1a4b000efb7bbcf86c
JDLSantos21 <JDLSantos21@hotmail.com>
2025-02-05 20:47:22 -0400

    updater work

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
 import { RoleBased } from "@/components/RoleBased";
+import { checkForUpdates } from "@/lib/update";
 
 export default function Dashboard() {
   return (
@@ -9,6 +10,8 @@ export default function Dashboard() {
         <h1>Dashboard</h1>
       </div>
 
+      <button onClick={checkForUpdates}>Check for updates</button>
+      <h1>Esta es la version 0.1.2</h1>
       {/* Panel de Administrador */}
       <RoleBased allowedRoles={["admin"]}>
         <div>
